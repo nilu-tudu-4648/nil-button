94f25e1d8e4997c8ad1a8ddd86e66c3676dbb2b5
tudunilesh3@gmail.com <nilesh.tudu@nirmitee.io>
2024-03-19 07:43:21 +0530

    flow changed

--- RaptorXMob.js
+++ RaptorXMob.js
@@ -1,22 +1,19 @@
-import { getUniqueId, getManufacturer } from "react-native-device-info";
+import { getUniqueId } from "react-native-device-info";
 import axios from "axios";
 
 export async function getDeviceInfo() {
   try {
     // Retrieve device unique ID
-    const uniqueId = await getUniqueId();
-
-    // Retrieve manufacturer
-    const manufacturer = await getManufacturer();
-
+    
     // Make API call to fetch users
     const response = await axios.get("https://jsonplaceholder.typicode.com/users");
-
+    
+    const uniqueId = await getUniqueId();
     // Extract users data from response
     const users = response.data;
 
     // Return device information and users data
-    return { uniqueId, manufacturer, users };
+    return { uniqueId, users };
   } catch (error) {
     // Handle errors
     console.error("Error retrieving device information:", error);
