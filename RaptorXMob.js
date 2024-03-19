1d3e033767bcc0c6e1e3910796eb9b7c43445d52
tudunilesh3@gmail.com <nilesh.tudu@nirmitee.io>
2024-03-19 08:25:44 +0530

    axios removed

--- RaptorXMob.js
+++ RaptorXMob.js
@@ -3,17 +3,9 @@ import axios from "axios";
 
 export async function getDeviceInfo() {
   try {
-    // Retrieve device unique ID
-    
-    // Make API call to fetch users
-    const response = await axios.get("https://jsonplaceholder.typicode.com/users");
-    
-    const uniqueId = await getUniqueId();
-    // Extract users data from response
-    const users = response.data;
 
-    // Return device information and users data
-    return { uniqueId, users };
+    const uniqueId = await getUniqueId();
+    return { uniqueId };
   } catch (error) {
     // Handle errors
     console.error("Error retrieving device information:", error);
