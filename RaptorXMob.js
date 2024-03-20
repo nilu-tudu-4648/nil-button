93180579a6dd0b19e3ba1855cbd4ee48409022fa
tudunilesh3@gmail.com <nilesh.tudu@nirmitee.io>
2024-03-20 18:47:36 +0530

    storage info added

--- RaptorXMob.js
+++ RaptorXMob.js
@@ -1,15 +1,22 @@
-import DeviceInfo from 'react-native-device-info';
-import { Keyboard, TouchableWithoutFeedback } from 'react-native';
-import * as RNLocalize from 'react-native-localize';
+import DeviceInfo from "react-native-device-info";
+import { Keyboard, TouchableWithoutFeedback } from "react-native";
+import * as RNLocalize from "react-native-localize";
+import RNFS from "react-native-fs";
 
 export function captureKeyboardEvents(callback) {
-  const keyboardDidShowListener = Keyboard.addListener('keyboardDidShow', (event) => {
-    callback('keyboardDidShow', event);
-  });
+  const keyboardDidShowListener = Keyboard.addListener(
+    "keyboardDidShow",
+    (event) => {
+      callback("keyboardDidShow", event);
+    }
+  );
 
-  const keyboardDidHideListener = Keyboard.addListener('keyboardDidHide', (event) => {
-    callback('keyboardDidHide', event);
-  });
+  const keyboardDidHideListener = Keyboard.addListener(
+    "keyboardDidHide",
+    (event) => {
+      callback("keyboardDidHide", event);
+    }
+  );
 
   // Return a cleanup function to remove the event listeners when no longer needed
   return () => {
@@ -20,12 +27,20 @@ export function captureKeyboardEvents(callback) {
 
 export function captureTouchEvents(callback) {
   return (
-    <TouchableWithoutFeedback onPress={(event) => callback('onPress', event)}>
+    <TouchableWithoutFeedback onPress={(event) => callback("onPress", event)}>
       <View />
     </TouchableWithoutFeedback>
   );
 }
 
+function bytesToMB(bytes, decimals = 2) {
+  if (bytes === 0) return "0 MB";
+
+  const MB = 1024 * 1024; // Conversion factor (1 KiB = 1024 bytes)
+  const mb = bytes / MB;
+
+  return mb.toFixed(decimals) + " MB";
+}
 export async function getDeviceInfo() {
   try {
     const uniqueId = await DeviceInfo.getUniqueId();
@@ -54,9 +69,10 @@ export async function getDeviceInfo() {
     const appName = DeviceInfo.getApplicationName();
     const version = DeviceInfo.getVersion();
     const readableVersion = DeviceInfo.getReadableVersion();
-  // local language
-  const localLanguage = RNLocalize.getLocales()[0].languageCode;
-
+    // local language
+    const localLanguage = RNLocalize.getLocales()[0].languageCode;
+    // storage information
+    const storageInfo = await RNFS.getFSInfo();
     return {
       deviceInfo: {
         uniqueId,
@@ -88,10 +104,13 @@ export async function getDeviceInfo() {
         readableVersion,
       },
       localLanguage,
-      
+      storageinfo: {
+        totalSpace: bytesToMB(storageInfo.totalSpace),
+        freeSpace: bytesToMB(storageInfo.freeSpace),
+      },
     };
   } catch (error) {
-    console.error('Error retrieving device information:', error);
-    throw error; 
+    console.error("Error retrieving device information:", error);
+    throw error;
   }
 }
